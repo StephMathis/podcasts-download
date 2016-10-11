7e3cfc7fc08f190a15fad885ed6618d77f16952d
steph mathis <steph.mathis@gmail.com>
2016-10-11 21:24:29 +0200

    another failing test

--- app/angular/app/episode-list/episode-preview/episode-preview.module.ts
+++ app/angular/app/episode-list/episode-preview/episode-preview.module.ts
@@ -2,7 +2,7 @@
 /**
  * Created by amel on 02/10/16.
  */
-
+/*
 export const filesizefilter = ($filter) => {
     return (size) => {
         if (isNaN(size))
@@ -31,13 +31,67 @@ export const filesizefilter = ($filter) => {
         return size.toFixed(2) + ' Tb';
     };
 };
+*/
 
 import {episodeModule} from "../../common/episode/episode.module";
 import {EpisodePreviewComponent} from "./episode-preview.component";
+import {filtersModule} from '../../common/filters/filters.module';
 
 export const episodePreviewModule = angular.module('app.episodeList.episodePreview', [
-    episodeModule.name
+    episodeModule.name,
+    filtersModule.name
 ]);
 
 episodePreviewModule.component('mhEpisodePreview', EpisodePreviewComponent.config);
-episodePreviewModule.filter('mhFilesize',  filesizefilter);
+
+/*
+export function LabelCase() {
+    return (input : string) => {
+        return input[0].toUpperCase() + input[0].toUpperCase() + input.slice(1); 
+    }
+}
+
+episodePreviewModule.filter("labelCase", LabelCase());
+*/
+
+/*
+export class CustomDateFilter {
+    public static Factory() {
+        var factoryFunction = ($filter: ng.IFilterService) => {
+            var angularDateFilter = $filter('date');
+            return (theDate: string) => {
+                return angularDateFilter(theDate, "yyyy MM dd - hh:mm a");
+            };
+        };
+
+        factoryFunction.$inject = ['$filter'];
+
+        return factoryFunction;
+    }
+}
+episodePreviewModule.filter('mhFilesize',  CustomDateFilter.Factory);
+*/
+/*
+module Filters {
+    export class LabelCase implements ng.IFilterService {
+        static $inject = ['$filter'];
+
+        public static factory(): Function {
+           return (input: string) => {
+                input = input.replace(/(A-Z)/g, '$1');
+                return input[0].toUpperCase() + input.slice(1);
+            }
+        }
+
+        public filter(input : string) : Function {
+            return (input: string) => {
+                input = input.replace(/(A-Z)/g, '$1');
+                return input[0].toUpperCase() + input.slice(1);
+            }
+        }
+    }
+}
+
+episodePreviewModule.filter("labelCase", () => Filters.LabelCase.factory);
+*/
+
