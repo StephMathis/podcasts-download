daf8b3c7ca30e4c70e5c9d177d9d8c1bc3d58c91
amel <am.guetat@gmail.com>
2016-10-30 22:18:21 +0100

    Mock angular material in podcast view test

--- test/karma/app/podcast-list/podcast-preview/podcast-preview.component.spec.ts
+++ test/karma/app/podcast-list/podcast-preview/podcast-preview.component.spec.ts
@@ -3,61 +3,77 @@
  */
 
 import * as $ from 'jquery';
+import 'angular-material/angular-material-mocks';
 import {podcastPreviewModule} from "../../../../../app/angular/app/podcast-list/podcast-preview/podcast-preview.module";
 import {PodcastContent, Podcast} from "../../../../../app/angular/app/common/podcast/podcast.component";
 
 
-xdescribe ('PodcastPreview', ()=>{
+fdescribe ('PodcastPreview', ()=>{
+
 
     beforeEach(angular.mock.module(podcastPreviewModule.name));
+    beforeEach(angular.mock.module('wishtack.templates'));
+    beforeEach(angular.mock.module('ngMaterial-mock'));
+
+    beforeEach(() =>{
+        angular.mock.module($provide => {
+            $provide.value('$mdDialog', {
+                show: jasmine.createSpy('show')
+            });
+        });
+        }
+    )
+
 
     beforeEach(inject(($compile,
-                       $http,
-                       $q,
                        $rootScope,
                        $templateCache,
-                       podcastStore) => {
+                       $mdDialog
+                       ) => {
 
         this.$compile = $compile;
-        this.$q = $q;
         this.$rootScope = $rootScope;
         this.$templateCache = $templateCache;
+        this.$mdDialogMock = $mdDialog;
         this.scope = this.$rootScope.$new();
 
-            }));
+        this.$templateCache.put(
+            require('../../../../../app/angular/app/podcast-list/podcast-preview/podcast-preview.component.html'),
+            require('!!raw-loader!../../../../../app/angular/app/podcast-list/podcast-preview/podcast-preview.component.html')
+        );
+    }));
+
 
     it('should display podcast', () => {
 
         let content = new PodcastContent(
             {
-                'link': 'http://www.france-info.com/',
-                'title': 'Chroniques du ciel',
-                'description': "Chaque dimanche, toute l'actualitÃ© Ã©conomique des compagnies aÃ©riennes, des constructeurs et des aÃ©roports, mais aussi les nouvelles technologies, les sports, l'aviation lÃ©gÃ¨re, le vol libre.",
-                'cover_url': 'http://media.radiofrance-podcast.net/podcast09/RF_OMM_0000014223_ITE.jpg'
+                link: 'http://www.france-info.com/',
+                title: 'Chroniques du ciel',
+                description: 'podcast description',
+                cover_url: 'http://media.radiofrance-podcast.net/podcast09/RF_OMM_0000014223_ITE.jpg'
             }
         )
 
-        let podcast = new Podcast({
-            "content": content,
-            "podcast_url": null,
-            "resource_uri": "/api/v1/podcasts/aHR0cDovL3JhZGlvZnJhbmNlLXBvZGNhc3QubmV0L3BvZGNhc3QwOS9yc3NfMTg5OTYueG1s/"
-        });
+        let podcastToInject: Podcast;
 
+        podcastToInject = new Podcast({
+            content: content,
+            podcast_url: "podcast ur",
+            podcast_id: "id"
+        });
 
         let rawElement;
         let element;
 
-        this.scope.podcast = podcast;
+        this.scope.podcast = podcastToInject;
 
         rawElement = this.$compile(`<mh-podcast-preview mh-podcast="podcast">`)(this.scope);
-        element = $(rawElement);
 
+        element = $(rawElement);
         this.scope.$apply();
 
-        console.log(element.html());
-
-        expect(element.find('.podcast-preview-title').text()).toEqual('Chroniques du ciel');
-
+        expect(element.find('.md-title').text()).toEqual('Chroniques du ciel');
 
     });
 })
