b564834a97c134cc6fc670f7f79e3e66335578b0
amel <am.guetat@gmail.com>
2016-10-30 22:23:13 +0100

    test input parameters

--- test/karma/app/episode-list/episode-list.component.spec.ts
+++ test/karma/app/episode-list/episode-list.component.spec.ts
@@ -3,3 +3,64 @@
  */
 
 
+import * as $ from 'jquery';
+import {Episode} from "../../../../app/angular/app/common/episode/episode";
+import {episodeListModule} from "../../../../app/angular/app/episode-list/episode-list.module";
+
+fdescribe ('EpisodeListModule', ()=>{
+
+    beforeEach(angular.mock.module(episodeListModule.name));
+    beforeEach(angular.mock.module('wishtack.templates'));
+
+
+    beforeEach(inject((episodeStore,
+                       $compile,
+                       $q,
+                       $rootScope,
+                       $templateCache
+
+    ) => {
+
+        this.$compile = $compile;
+        this.$q = $q;
+        this.$rootScope = $rootScope;
+        this.$templateCache = $templateCache;
+        this.scope = this.$rootScope.$new();
+        this.scope.episodeStore = episodeStore;
+
+
+        this.$templateCache.put(
+            require('../../../../app/angular/app/episode-list/episode-list.component.html'),
+            require('!!raw-loader!../../../../app/angular/app/episode-list/episode-list.component.html')
+        );
+
+        let episodeJson = {"duration":125,
+            "episode_id":"episodeid123",
+            "size":114455,
+            "source_url":"http://here.dot/aaa.mp3",
+            "subtitle":"subtitle episode",
+            "title":"title episode"};
+
+        let episode : Episode = new Episode(episodeJson);
+        spyOn(this.scope.episodeStore, 'episodeList').and.returnValue(this.$q.resolve([episode]));
+
+    }));
+
+
+
+    it('should display episode-list', () => {
+
+        let rawElement;
+        let element;
+
+        this.scope.podcastIdInjected = "123456";
+
+        rawElement = this.$compile(`<mh-episode-list mh-podcast-id="podcastIdInjected"></mh-episode-list>`)(this.scope);
+        this.scope.$apply();
+
+        element = $(rawElement);
+
+        //expect(element.html()).toEqual("labore eiusmod aliquip consectetur");
+        expect(this.scope.episodeStore.episodeList.calls.count()).toBe(1);
+        expect(this.scope.episodeStore.episodeList).toHaveBeenCalledWith(this.scope.podcastIdInjected);
+    })})
