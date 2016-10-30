45f49250f409141267ed756ad00b83bef9d61b4c
steph mathis <steph.mathis@gmail.com>
2016-10-30 17:35:48 +0100

    adding test on episode-list, episode-list was slightly modified

--- test/karma/app/episode-list/episode-list.component.spec.ts
+++ test/karma/app/episode-list/episode-list.component.spec.ts
@@ -2,4 +2,73 @@
  * Created by amel on 04/10/16.
  */
 
+import * as $ from 'jquery';
+import {Episode} from "../../../../app/angular/app/common/episode/episode";
+import {EpisodeStore} from "../../../../app/angular/app/common/episode/episode-store";
+import {RestangularResources} from "../../../../app/angular/app/common/restangular/resources/restangular.resources";
+import {episodeListModule} from "../../../../app/angular/app/episode-list/episode-list.module";
 
+fdescribe ('EpisodeListModule', ()=>{
+
+    beforeEach(angular.mock.module(episodeListModule.name));
+    beforeEach(angular.mock.module('wishtack.templates'));
+
+    beforeEach(inject((episodeStore,
+                       $compile,
+                       $q,
+                       $rootScope,
+                       $templateCache
+                       ) => {
+
+        this.$compile = $compile;
+        this.$q = $q;
+        this.$rootScope = $rootScope;
+        this.$templateCache = $templateCache;
+        this.scope = this.$rootScope.$new();
+        this.scope.episodeStore = episodeStore;
+        
+        this.$templateCache.put(
+            require('../../../../app/angular/app/episode-list/episode-list.component.html'),
+            require('!!raw-loader!../../../../app/angular/app/episode-list/episode-list.component.html')
+        );
+
+        let episodeJson = {"duration":125,
+            "episode_id":"episodeid123",
+            "size":114455,
+            "source_url":"http://here.dot/aaa.mp3",
+            "subtitle":"subtitle episode",
+            "title":"title episode"};
+
+        let episode : Episode = new Episode(episodeJson);
+        //this.scope.episodeStore = new EpisodeStore(this.$q,null,null,null);
+        spyOn(this.scope.episodeStore, 'episodeList').and.returnValue(this.$q.resolve([episode]));
+
+    }));
+
+
+    it('should display episode-list', () => {
+
+        let rawElement;
+        let element;
+
+        console.log("0");
+        this.scope.podcastIdInjected = "123456";
+
+        console.log("1");
+        rawElement = this.$compile(`<mh-episode-list mh-podcast-id="podcastIdInjected"></mh-episode-list>`)(this.scope);
+
+        console.log("2");
+        this.scope.$apply();
+
+        console.log("3");
+        element = $(rawElement);
+
+        console.log("4");
+        //expect(element.html()).toEqual("labore eiusmod aliquip consectetur");
+        expect(this.scope.episodeStore.episodeList.calls.count()).toBe(1);
+        expect(this.scope.episodeStore.episodeList).toHaveBeenCalledWith(this.scope.podcastIdInjected);
+
+
+        expect('Chroniques du ciel').toEqual('Chroniques du ciel');
+    });
+})
--- test/karma/app/podcast-list/podcast-preview/podcast-preview.component.spec.ts
+++ test/karma/app/podcast-list/podcast-preview/podcast-preview.component.spec.ts
@@ -2,29 +2,35 @@
  * Created by amel on 25/09/16.
  */
 
+import 'angular-material';
 import * as $ from 'jquery';
 import {podcastPreviewModule} from "../../../../../app/angular/app/podcast-list/podcast-preview/podcast-preview.module";
 import {PodcastContent, Podcast} from "../../../../../app/angular/app/common/podcast/podcast.component";
 
-
-xdescribe ('PodcastPreview', ()=>{
+describe ('PodcastPreview', ()=>{
 
     beforeEach(angular.mock.module(podcastPreviewModule.name));
 
     beforeEach(inject(($compile,
-                       $http,
-                       $q,
+                       $mdDialog,
                        $rootScope,
-                       $templateCache,
-                       podcastStore) => {
+                       $templateCache
+                       ) => {
 
         this.$compile = $compile;
-        this.$q = $q;
         this.$rootScope = $rootScope;
         this.$templateCache = $templateCache;
         this.scope = this.$rootScope.$new();
+        this.$mdDialog = $mdDialog;
 
-            }));
+
+
+        this.$templateCache.put(
+            require('../../../../../app/angular/app/podcast-list/podcast-preview/podcast-preview.component.html'),
+            require('!!raw-loader!../../../../../app/angular/app/podcast-list/podcast-preview/podcast-preview.component.html')
+        );
+
+    }));
 
     it('should display podcast', () => {
 
@@ -43,20 +49,29 @@ xdescribe ('PodcastPreview', ()=>{
             "resource_uri": "/api/v1/podcasts/aHR0cDovL3JhZGlvZnJhbmNlLXBvZGNhc3QubmV0L3BvZGNhc3QwOS9yc3NfMTg5OTYueG1s/"
         });
 
-
         let rawElement;
         let element;
 
+        console.log("0");
+
         this.scope.podcast = podcast;
 
+        console.log("1");
+
         rawElement = this.$compile(`<mh-podcast-preview mh-podcast="podcast">`)(this.scope);
+
+        console.log("2");
         element = $(rawElement);
+
+        console.log("3");
 
         this.scope.$apply();
 
+        console.log("4");
+
         console.log(element.html());
 
-        expect(element.find('.podcast-preview-title').text()).toEqual('Chroniques du ciel');
+        expect(element.find('.md-title').text()).toEqual('Chroniques du ciel');
 
 
     });
