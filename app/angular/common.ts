7a02c0600f214e4f06c71201c1f066cc85173548
steph mathis <steph.mathis@gmail.com>
2016-11-11 22:37:17 +0100

    first animation transition but with a unwanted effect when go back

--- app/angular/common.ts
+++ app/angular/common.ts
@@ -11,5 +11,7 @@ import 'core-js/es7/reflect';
 import 'lodash';
 import 'restangular';
 import 'angular-material';
+import 'angular-animate';
 import 'angular-local-storage';
 
+require('!!style!css!./podcasts.css');
